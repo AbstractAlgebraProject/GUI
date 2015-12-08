2f3416a4b20e513db773a8b1cea38fa6e8c9bd58
sheriffsoco <japenes@gmail.com>
2015-12-08 09:51:10 -0800

    Add dynamic drop downs
    - Can add items

--- testProject/js/mainCode.js
+++ testProject/js/mainCode.js
@@ -1,1 +1,12 @@
 // This code is the actual program (Nothing Here Yet! Learning Javascript and Bootstrap!)
+function addItems(idName, textValue) {
+    $(idName).append( '<li><a href="#">' + textValue + '</a></li>'); 
+    //This adds a new list item with text
+}
+
+$('#dropButton').on('click', function (e){ //When button is clicked
+    var inputText = document.getElementById('searchBar').value; //This gets value from <input>
+    if(inputText != ""){
+        addItems("#drop", inputText);
+    }
+});
