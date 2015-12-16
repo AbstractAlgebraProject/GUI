425a556374952aad734d4597abdb413260d70ab7
sheriffsoco <japenes@gmail.com>
2015-12-16 08:23:47 -0800

    Create Dropdowns using Javascript

--- testProject/js/mainCode.js
+++ testProject/js/mainCode.js
@@ -1,12 +1,29 @@
-// This code is the actual program (Nothing Here Yet! Learning Javascript and Bootstrap!)
 function addItems(idName, textValue) {
-    $(idName).append( '<li><a href="#">' + textValue + '</a></li>'); 
+    $(idName).append('<li><a href="#">' + textValue + '</a></li>'); 
     //This adds a new list item with text
 }
 
-$('#dropButton').on('click', function (e){ //When button is clicked
+function addDropdown(navbarId, title, dropdownId) {
+    $(navbarId).append('<li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + title +  '<span class="caret"></span></a> <ul class="dropdown-menu" id="' + dropdownId + '"> </ul> </li>'); 
+}
+
+$(addItems("#drop", "hi"));
+$(addDropdown("#dynamicNav", "Test", "Thing"));
+$(addItems("#Thing", "hello"));
+$(addItems("#Thing", "world"));
+
+/*$('#dropButton').on('click', function (e){ //When button is clicked
     var inputText = document.getElementById('searchBar').value; //This gets value from <input>
     if(inputText != ""){
         addItems("#drop", inputText);
     }
 });
+
+$(function() {
+    var change = function( txt ) {
+        $("#ID").append( '<li>' + txt + '</li>' );
+    };
+    change("this is the first change");
+    change("this is the second change");
+});*/
+
